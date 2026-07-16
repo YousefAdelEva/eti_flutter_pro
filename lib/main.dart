97d81f4c2f52c907b1c1584b63853386fbac6d75
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2026-07-16 14:18:57 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hossam Hesham Issa',
+      title: 'Bavly',
       theme: ThemeData(
         // This is the theme of your application.
         //
