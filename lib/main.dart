30db376e69deafaf709a09d2719a604ae31c40ce
Hossam <hossamhesham413@gmail.com>
2026-07-16 14:15:49 +0300

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter pro',
+      title: 'Hossam Hesham Issa',
       theme: ThemeData(
         // This is the theme of your application.
         //
